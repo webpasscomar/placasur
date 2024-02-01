b70280841f74b8d403373e1707cded7d84668404
Eduardo Passarelli <epassarelli@gmail.com>
2024-02-01 13:40:20 -0300

    Produccion 2024-02-01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,17 +12,16 @@
   <!-- .ico -->
   <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" type="image/x-icon">
   <title>{{ config('app.name', 'PlacaSUR') }} - @yield('title')</title>
-
+  <!-- CSS - bs - custom -->
+  <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+  <!-- fontawesome iconos -->
+  <link href="{{ asset('fontawesome/css/all.css') }}" rel="stylesheet">
+  <link href="{{ asset('fontawesome/css/fontawesome.css') }}" rel="stylesheet">
+  <link href="{{ asset('fontawesome/css/brands.css') }}" rel="stylesheet">
+  <link href="{{ asset('fontawesome/css/solid.css') }}" rel="stylesheet">
   <!-- Scripts -->
-  @vite(['resources/sass/app.scss', 
-  'public/fontawesome/css/all.css', 
-  'public/fontawesome/css/fontawesome.css', 
-  'public/fontawesome/css/brands.css', 
-  'public/fontawesome/css/solid.css', 
-  'public/css/custom.css', 
-  'resources/js/app.js', 
-  'public/js/jquery.min.js', 
-  'public/js/bootstrap.bundle.min.js'])
+  @vite(['resources/sass/app.scss', 'public/fontawesome/css/all.css', 'public/fontawesome/css/fontawesome.css', 'public/fontawesome/css/brands.css', 'public/fontawesome/css/solid.css', 'public/css/custom.css', 'resources/js/app.js', 'public/js/jquery.min.js', 'public/js/bootstrap.bundle.min.js'])
 </head>
 
 <body>
