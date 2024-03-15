42e9988a64143e053bd593736fb6b79a4988b689
Eduardo Passarelli <epassarelli@gmail.com>
2024-03-15 09:42:52 -0300

    Revision de estilos con Juli

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,30 +2,50 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Placasur">
-    <meta name="author" content="Placasur SA">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <!-- .ico -->
-    <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" type="image/x-icon">
-    <title>{{ config('app.name', 'PlacaSUR') }} - @yield('title')</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Placasur">
+  <meta name="author" content="Placasur SA">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <!-- .ico -->
+  <link rel="shortcut icon" href="{{ asset('favicons/favicon.ico') }}" type="image/x-icon">
+  <title>{{ config('app.name', 'PlacaSUR') }} - @yield('title')</title>
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'public/fontawesome/css/all.css', 'public/fontawesome/css/fontawesome.css', 'public/fontawesome/css/brands.css', 'public/fontawesome/css/solid.css', 'public/css/custom.css', 'resources/js/app.js', 'public/js/jquery.min.js', 'public/js/bootstrap.bundle.min.js'])
+  <!-- Scripts -->
+  {{-- @vite(['resources/sass/app.scss', 'public/fontawesome/css/all.css', 'public/fontawesome/css/fontawesome.css', 'public/fontawesome/css/brands.css', 'public/fontawesome/css/solid.css', 'public/css/custom.css', 'resources/js/app.js', 'public/js/jquery.min.js', 'public/js/bootstrap.bundle.min.js']) --}}
+
+  <!-- CSS - bs - custom -->
+  <link href="https://placasur.webpass.com.ar/css/estilos.css" rel="stylesheet">
+  <link href="https://placasur.webpass.com.ar/css/custom.css" rel="stylesheet">
+  <!-- fontawesome iconos -->
+  <link href="https://placasur.webpass.com.ar/fontawesome/css/all.css" rel="stylesheet">
+  <link href="https://placasur.webpass.com.ar/fontawesome/css/fontawesome.css" rel="stylesheet">
+  <link href="https://placasur.webpass.com.ar/fontawesome/css/brands.css" rel="stylesheet">
+  <link href="https://placasur.webpass.com.ar/fontawesome/css/solid.css" rel="stylesheet">
+  <!-- Scripts -->
+  {{-- <link rel="preload" as="style" href="https://placasur.webpass.com.ar/build/assets/app-298ba296.css" />
+  <link rel="modulepreload" href="https://placasur.webpass.com.ar/build/assets/app-d0eb4253.js" />
+  <link rel="stylesheet" href="https://placasur.webpass.com.ar/build/assets/app-298ba296.css" />
+  <script type="module" src="https://placasur.webpass.com.ar/build/assets/app-d0eb4253.js"></script> --}}
+
+  {{-- @vite(['resources/sass/app.scss', 'public/fontawesome/css/all.css', 'public/fontawesome/css/fontawesome.css', 'public/fontawesome/css/brands.css', 'public/fontawesome/css/solid.css', 'public/css/custom.css', 'resources/js/app.js', 'public/js/jquery.min.js', 'public/js/bootstrap.bundle.min.js']) --}}
+
+</head>
+
+
 </head>
 
 <body>
 
-    @include('layouts.partials.header')
+  @include('layouts.partials.header')
 
-    <div class="min-vh-100">
-        @yield('content')
-    </div>
+  <div class="min-vh-100">
+    @yield('content')
+  </div>
 
-    @include('layouts.partials.footer')
+  @include('layouts.partials.footer')
 
 </body>
 
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,29 +1,35 @@
 <!-- NAV / menú -->
 <header class="sticky-md-top border-top border-5 border-primary">
-    <nav class="container-fluid navbar navbar-expand-lg bg-white py-0 menutop shadow">
-        <div class="container-md">
-            <a class="navbar-brand col-6 col-md-3 col-lg-3" href="{{ route('home') }}" title="Inicio Placa Sur">
-                <!-- logo -->
-                <img src="{{ asset('img/placasur.png') }}" alt=" Placa Sur" class="img-fluid float-left">
-                <h1 class="visually-hidden"> Placa Sur</h1>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item {{ request()->routeIS('empresa') ? 'active' : '' }}">
-                        <a href="{{ route('empresa') }}" title="Página principal" class="nav-link">Empresa <span
-                                class="visually-hidden">(Home)</span></a>
-                    </li>
-                    <li class="nav-item {{ request()->routeIS('servicios') ? 'active' : '' }}">
-                        <a href="{{ route('servicios') }}" title="Nuestros servicios" class="nav-link">Servicios</a>
-                    </li>
-                    <li class="nav-item {{ request()->routeIS('productos') ? 'active' : '' }}">
-                        <a href="{{ route('productos') }}" title="Nuestros productos" class="nav-link">Productos</a>
-                    </li>
-                    {{-- <li class="nav-item dropdown {{ request()->routeIS('productos') ? 'active' : '' }}">
+
+  <div class="row bg-danger">
+    <div class="col-md-12 pt-5 pb-3">
+    </div>
+  </div>
+
+  <nav class="container-fluid navbar navbar-expand-lg bg-white py-0 menutop shadow">
+    <div class="container-md">
+      <a class="navbar-brand col-6 col-md-3 col-lg-3" href="{{ route('home') }}" title="Inicio Placa Sur">
+        <!-- logo -->
+        <img src="{{ asset('img/placasur.png') }}" alt=" Placa Sur" class="img-fluid float-left">
+        <h1 class="visually-hidden"> Placa Sur</h1>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item {{ request()->routeIS('empresa') ? 'active' : '' }}">
+            <a href="{{ route('empresa') }}" title="Página principal" class="nav-link">Empresa <span
+                class="visually-hidden">(Home)</span></a>
+          </li>
+          <li class="nav-item {{ request()->routeIS('servicios') ? 'active' : '' }}">
+            <a href="{{ route('servicios') }}" title="Nuestros servicios" class="nav-link">Servicios</a>
+          </li>
+          <li class="nav-item {{ request()->routeIS('productos') ? 'active' : '' }}">
+            <a href="{{ route('productos') }}" title="Nuestros productos" class="nav-link">Productos</a>
+          </li>
+          {{-- <li class="nav-item dropdown {{ request()->routeIS('productos') ? 'active' : '' }}">
                       <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
                         Productos
@@ -39,30 +45,30 @@
                       </div>
                     </li> --}}
 
-                    <li class="nav-item {{ request()->routeIS('novedades') ? 'active' : '' }}">
-                        <a href="{{ route('novedades') }}" title="Nuestras actividades" class="nav-link">Novedades</a>
-                    </li>
-                    <!-- contacto -->
-                    <li class="nav-item {{ request()->routeIS('contacto') ? 'active' : '' }}">
-                        <a href="{{ route('contacto') }}" title="Contactanos" class="nav-link">Contacto</a>
-                    </li>
-                    <!-- buscador -->
-                    <li class="nav-item search">
-                        <form>
-                            <div class="animated-search m-md-0 mb-sm-4">
-                                <input type="search" id="animated-input">
-                                <a href="#">
-                                    <i class="fas fa-search" id="searchBtn"></i>
-                                </a>
-                            </div>
-                        </form>
-                    </li>
-                </ul>
-            </div>
-            </li>
+          <li class="nav-item {{ request()->routeIS('novedades') ? 'active' : '' }}">
+            <a href="{{ route('novedades') }}" title="Nuestras actividades" class="nav-link">Novedades</a>
+          </li>
+          <!-- contacto -->
+          <li class="nav-item {{ request()->routeIS('contacto') ? 'active' : '' }}">
+            <a href="{{ route('contacto') }}" title="Contactanos" class="nav-link">Contacto</a>
+          </li>
+          <!-- buscador -->
+          <li class="nav-item search">
+            <form>
+              <div class="animated-search m-md-0 mb-sm-4">
+                <input type="search" id="animated-input">
+                <a href="#">
+                  <i class="fas fa-search" id="searchBtn"></i>
+                </a>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </div>
+      </li>
 
-            </ul>
-        </div>
-        </div>
-    </nav>
+      </ul>
+    </div>
+    </div>
+  </nav>
 </header>
