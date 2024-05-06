90bea71707ac48463580a7185d038a1f747525de
lean-2021 <leanw1711@gmail.com>
2024-05-06 17:16:41 -0300

    Validación y envío de formulario de contacto

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -1,6 +1,6 @@
 <?php
 
-Namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
@@ -27,13 +27,34 @@ class ContactoController extends Controller
     {
         // Validar los datos del formulario
         $validatedData = $request->validate([
-            'nombre' => 'required|string',
-            'apellido' => 'required|string',
-            'email' => 'required|email',
-            'asunto' => 'required|numeric',
-            'telefono' => 'nullable|string',
-            'rubro' => 'nullable|numeric',
-            'mensaje' => 'required|string',
+            'nombre' => 'required|string|min:3|max:50|regex:/^[\pL\s\-]+$/u|not_regex:/^[\s]*$/u',
+            'apellido' => 'required|string|min:3|max:50|regex:/^[\pL\s\-]+$/u|not_regex:/^[\s]*$/u',
+            'email' => 'required|email|regex:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/',
+            'asunto' => 'required',
+            'telefono' => 'nullable|regex:/^[0-9]+$/|digits_between:7,15',
+            'rubro' => 'nullable',
+            'mensaje' => 'required|string|min:10|max:500',
+        ], [
+            'nombre.required' => 'Debe ingresar un nombre',
+            'nombre.min' => 'Ingrese al menos 3 caracteres',
+            'nombre.max' => 'No puede ingresar más de 50 caracteres',
+            'nombre.regex' => 'Ingrese un nombre válido',
+            'nombre.not_regex' => 'Ingrese un nombre válido',
+            'apellido.required' => 'Debe ingresar el apellido',
+            'apellido.min' => 'Ingrese al menos 3 caracteres',
+            'apellido.max' => 'No puede ingresar más de 50 caracteres',
+            'apellido.regex' => 'Ingrese un apellido válido',
+            'apellido.not_regex' => 'Ingrese un apellido válido',
+            'email.required' => 'Debe ingresar un correo electronico',
+            'email.email' => 'Ingrese un correo electronico válido',
+            'email.regex' => 'Ingrese un correo electronico válido',
+            'asunto.required' => 'Debe seleccionar un asunto',
+            'telefono.regex' => 'Ingrese un telefono válido',
+            'telefono.digits_between' => 'El teléfono debe tener entre 7 y 15 dígitos',
+            'mensaje.required' => 'Debe escribir un mensaje',
+            'mensaje.min' => 'Ingrese al menos 10 caracteres',
+            'mensaje.max' => 'No puede escribir más de 500 caracteres',
+            'mensaje.regex' => 'Ingrese un mensaje válido',
         ]);
 
         // Configurar destinatarios de correo basados en el asunto seleccionado
@@ -54,9 +75,9 @@ class ContactoController extends Controller
         }
 
         // Envía el correo
-        Mail::send('contacto', ['data' => $validatedData], function ($message) use ($validatedData, $to) {
+        Mail::send('contacto-email', ['data' => $validatedData], function ($message) use ($validatedData, $to) {
             $message->to($to);
-            $message->asunto('Nuevo mensaje de contacto: ' . $validatedData['asunto']);
+            $message->subject('Nuevo mensaje de contacto: ' . $validatedData['asunto']);
         });
 
         // Redirige de vuelta al formulario con un mensaje de éxito
